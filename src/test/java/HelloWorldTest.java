71b9a0a7c214401450c87b236ffee3b3fe468aaf
karthiksuresha <89825873+karthiksuresha@users.noreply.github.com>
2021-12-16 15:54:45 +0530

    Add files via upload

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,7 @@
+public class HelloWorldTest {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld.say("Hi"));
+    }
+
+}
